4c575788e92a1687ca59e370a592f8297d1cf5b0
Caiottk <caionarutolegal@gmail.com>
2024-10-23 22:43:51 -0300

    Rascunho inicial publisher e subscriber

---
+++ atividade02/publisher.py
@@ -0,0 +1,29 @@
+import pika
+import argparse
+
+
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', default="localhost", action='store', help='RabbitMQ host')
+    parser.add_argument('--exchange', default="default_pc", action='store', help='RabbitMQ exchange')
+    return parser
+
+def get_connetion_channel(host, exchange):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+    channel = connection.channel()
+    channel.exchange_declare(exchange=exchange)
+    return connection, channel
+
+def main(host, exchange):
+    connection, channel = get_connetion_channel(host, exchange)
+    try:
+        print("A fazer")
+    finally:
+        connection.close()
+
+if __name__ == "__main__":
+    parser = get_args()
+    args = parser.parse_args()
+    main(args.host, args.exchange)
