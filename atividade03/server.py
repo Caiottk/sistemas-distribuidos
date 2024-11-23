b199827d2367d1fe739553f6c865ae69aa2ddce3
FabioFukuda <fsfkd@outlook.com>
2024-11-23 18:19:49 -0300

    Votante encontrando a URI do leader e se registrando

--- atividade03/server.py
+++ atividade03/server.py
@@ -3,39 +3,44 @@ from threading import Timer
 import time
 
 @Pyro5.api.expose
-class Server:
+class Leader:
     def __init__(self):
-        self.__notification_uris = []
+        self.voter_uris = []
+        self.observer_uris = []
+
         self.log = []
         self.quorum_size = 2
         self.confirmations = {}
         self.heart_interval = {}
+        
+        self.daemon = Pyro5.server.Daemon()
+        self.uri = self.daemon.register(self)
+
+        ns = Pyro5.api.locate_ns()
+        ns.register('Líder-Epoca1', self.uri)
+        
+    def start(self):
+        print('Leader is running...')
+        self.daemon.requestLoop()
 
     def register_voter(self, uri):
-        self.__notification_uris.append(uri)
+        self.voter_uris.append(uri)
+        print(f"Novo votante registrado. URI: {uri}")
+        return True
+    
+    def register_observer(self, uri):
+        self.observer_uris.append(uri)
+        print(f"Novo observador registrado. URI: {uri}")
+        return True
     
     def add_entry(self, entry):
         self.log.append(entry)
         entry_offset = len(self.log) - 1
         print (f"Entry added: {entry} at index {entry_offset}")
 
-    
-
+    def hello(self):
+        print("Hello")
 
 if __name__ == '__main__':
-    try:
-        server = Server()
-        daemon = Pyro5.server.Daemon()
-        ns = Pyro5.api.locate_ns()
-
-        uri = daemon.register(server)
-        ns.register('Líder-Epoca1', uri)
-
-        print('Server is running...')
-
-        heart_beat_thread = Timer(0, server.monitorar_heartbets)
-        heart_beat_thread.start()
-
-        daemon.requestLoop()
-    finally:
-        daemon.close()
+    leader = Leader()
+    leader.start()
--- atividade03/voter.py
+++ atividade03/voter.py
@@ -1,8 +1,27 @@
 import Pyro5.api
 
+@Pyro5.api.expose
 class Voter:
     def __init__(self):
+        self.uri = None
+
         self.__notification_uris = []
+
+        self.daemon = Pyro5.server.Daemon()
+        self.uri = self.daemon.register(self)
+
+        name_server = Pyro5.api.locate_ns()
+        self.leader_uri = name_server.lookup('Líder-Epoca1')  
+        self.leader = Pyro5.api.Proxy(self.leader_uri)
+    
+        if not self.leader.register_voter(self.uri):
+            raise("Falha ao se registrar como votante.")
+        else:
+            print("Registrado como votante com sucesso.")
+            
+    def start(self):
+        print('Voter is running...')
+        self.daemon.requestLoop()
 
     def add_notification_uri(self, uri):
         self.__notification_uris.append(uri)
@@ -15,3 +34,10 @@ class Voter:
             with Pyro5.api.Proxy(uri) as observer:
                 observer.update(message)
 
+    def setUri(self,uri):
+        self.uri = uri
+
+if __name__ == '__main__':
+    voter = Voter()
+    voter.start()
+    
